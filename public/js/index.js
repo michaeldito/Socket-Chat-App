9d8e081a288736558cb8772dfdcd0a030e95adeb
Michael Dito <mcdito13@gmail.com>
2018-03-19 20:56:59 -0700

    styles, timestamps, location

--- public/js/index.js
+++ public/js/index.js
@@ -1,15 +1,10 @@
 // This is our client side javascript
+
 var socket = io();
 
 // this event fires when the connection starts
 socket.on('connect', function () {
   console.log('Connected to server');
-
-  // emit a message to the server
-  socket.emit('createMessage', {
-    from: 'client',
-    text: 'Yo from client!'
-  });
 });
 
 // this event fires when the connection drops
@@ -18,7 +13,57 @@ socket.on('disconnect', function () {
 });
 
 socket.on('newMessage', function (message) {
+  let formattedTime = moment(message.createdAt).format('h:mm a');
   console.log('newMessage:', message);
+  let li = jQuery('<li></li>');
+  li.text(`${message.from} ${formattedTime}: ${message.text}`);
+
+  jQuery('#messages').append(li);
+});
+
+socket.on('newLocationMessage', function (message) {
+  let formattedTime = moment(message.createdAt).format('h:mm a');
+  let li = jQuery('<li></li>');
+  let a = jQuery('<a target="_blank">My current location</a>');
+
+  li.text(`${message.from} ${formattedTime}: `);
+  a.attr('href', message.url);
+  li.append(a);
+  jQuery('#messages').append(li);
+});
+
+jQuery('#message-form').on('submit', function (e) {
+  e.preventDefault();
+
+  let messageTextBox = jQuery('[name=message]');
+
+  socket.emit('createMessage', {
+    from: 'User',
+    text: messageTextBox.val()
+  }, function() {
+    messageTextBox.val('');
+  });
+});
+
+// click listener
+var locationButton = jQuery('#send-location');
+locationButton.on('click', function () {
+  if (!navigator.geolocation) {
+    return alert('Geolocation not supported by your browser.');
+  }
+
+  locationButton.attr('disabled', 'disabled');
+
+  navigator.geolocation.getCurrentPosition(function (position) {
+    locationButton.removeAttr('disabled').text('Send location'); 
+    socket.emit('createLocationMessage', {
+      latitude: position.coords.latitude,
+      longitude: position.coords.longitude
+    });
+  }, function () {
+    locationButton.removeAttr('disabled').text('Send location');    
+    alert('Unable to fetch location.');
+  });
 });
 
 // we can't use arrow functions because some browsers can't 
--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
 
+const { generateMessage, generateLocationMessage } = require('./utils/message');
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 let app = express();
@@ -17,15 +18,21 @@ io.on('connection', (socket) => {
   console.log('New user connected');
 
   // emit a custom event to the client side
-  socket.emit('newMessage', {
-    from: 'server',
-    text: 'Yo from server!',
-    createdAt: new Date()
-  });
+  socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app!'));
+  
+  // emit a message to every connection, while io.emit 
+  // is just for one connection.
+  socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined'));
 
   // listen for createMessage events
-  socket.on('createMessage', (message) => {
+  socket.on('createMessage', (message, callback) => {
     console.log('createMessage', message);
+    io.emit('newMessage', generateMessage(message.from, message.text));
+    callback('This is from the server.');
+  });
+
+  socket.on('createLocationMessage', (coords) => {
+    io.emit('newLocationMessage', generateLocationMessage('Admin', coords.latitude, coords.longitude));
   });
 
   // Listen for when the client disconnects
