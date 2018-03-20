4446918b12a3a7ccb003ac774c7ad76428e3c97c
Michael Dito <mcdito13@gmail.com>
2018-03-19 21:34:05 -0700

    scroll to bottom

--- public/js/index.js
+++ public/js/index.js
@@ -2,6 +2,22 @@
 
 var socket = io();
 
+function scrollToBottom () {
+  // Selectors
+  let messages = jQuery('#messages');
+  let newMessage = messages.children('li:last-child');
+  // Heights
+  let clientHeight = messages.prop('clientHeight');
+  let scrollTop = messages.prop('scrollTop');
+  let scrollHeight = messages.prop('scrollHeight');
+  let newMessageHeight = newMessage.innerHeight();
+  let lastMessageHeight = newMessage.prev().innerHeight();
+
+  if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
+    messages.scrollTop(scrollHeight);
+  }
+}
+
 // this event fires when the connection starts
 socket.on('connect', function () {
   console.log('Connected to server');
@@ -22,6 +38,7 @@ socket.on('newMessage', function (message) {
   });
 
   jQuery('#messages').append(html);
+  scrollToBottom();
 });
 
 socket.on('newLocationMessage', function (message) {
@@ -32,8 +49,9 @@ socket.on('newLocationMessage', function (message) {
     url: message.url,
     createdAt: formattedTime
   });
-  
+
   jQuery('#messages').append(html);
+  scrollToBottom();
 });
 
 jQuery('#message-form').on('submit', function (e) {
